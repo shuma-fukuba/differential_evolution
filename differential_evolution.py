d0258605c67fd250597e6ffc4e24d43fe8cef845
shuma-fukuba <fukuba-shuma-dn@ynu.jp>
2022-11-01 15:26:17 +0900

    target-best1

--- differential_evolution.py
+++ differential_evolution.py
@@ -25,11 +25,17 @@ class DifferentialEvolution(GeneticAlgorithm):
             任意の個体を２つ選んで、new solutionを更新させる
             """
             # 親個体3つをランダムに選んで変異させてみる
-            parent_1, parent_2, parent_3 =random.sample(
-                self.solutions, 3)
-            mutated_solution = self.mutation(target_solution=parent_1,
-                                             solution_2=parent_2,
-                                             solution_3=parent_3)
+            # parent_1, parent_2, parent_3 = random.sample(
+            #     self.solutions, 3)
+            # mutated_solution = self.mutation(target_solution=parent_1,
+            #                                  solution_2=parent_2,
+            #                                  solution_3=parent_3)
+            best_solution = self.get_best_solution()
+            rand_solution_1, rand_solution_2 = random.sample(self.solutions, 2)
+            mutated_solution = self.target_best1_mutation(solution_i=solution,
+                                                          best_solution=best_solution,
+                                                          rand_solution_1=rand_solution_1,
+                                                          rand_solution_2=rand_solution_2)
 
             # 交叉
             new_solution = self.crossover(solution, mutated_solution)
@@ -48,6 +54,23 @@ class DifferentialEvolution(GeneticAlgorithm):
         mutated.f = target_solution.f
         mutated.x = target_solution.x + self.config.F * \
             (np.array(solution_2.x) - np.array(solution_3.x))
+        self.get_fitness(mutated)
+        return mutated
+
+    def target_best1_mutation(self,
+                              solution_i: Solution,
+                              best_solution: Solution,
+                              rand_solution_1: Solution,
+                              rand_solution_2: Solution) -> Solution:
+        self.get_fitness(solution_i)
+        mutated = Solution(config=self.config, function=self.function)
+
+        mutated.f = solution_i.f
+        mutated.x = solution_i.x \
+            + self.config.F * (np.array(best_solution.x) -
+                               np.array(solution_i.x)) \
+            + self.config.F * (np.array(rand_solution_1.x) -
+                               np.array(rand_solution_2.x))
         self.get_fitness(mutated)
         return mutated
 
@@ -84,6 +107,6 @@ class DifferentialEvolution(GeneticAlgorithm):
             self.solutions.append(offspring)
             self.solutions.remove(parent)
         # 親が優れている場合は何もしないでok
-    
+
     def get_best_solution(self) -> Solution:
         return min(self.solutions, key=lambda x: x.f)
