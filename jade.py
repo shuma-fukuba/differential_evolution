61d7e04d49ffbe0837ec978d6136dc5a01e5c24b
fukuba-shuma <calculus.fukuba@gmail.com>
2022-11-07 09:53:16 +0900

    delete unnecessary file

--- jade.py
+++
@@ -1,9 +0,0 @@
-from differential_evolution import DifferentialEvolution
-
-
-class Jade(DifferentialEvolution):
-    def get_next_population(self):
-        '''
-        パラメータを更新していく
-        初期値は設定しておいて、
-        '''
